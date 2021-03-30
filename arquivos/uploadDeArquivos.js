9d5e565dc66c458469157a206fe7e93a81112292
Willian Faria <william.faria@zukk.com.br>
2021-03-30 20:02:45 -0300

    Tratamento de imagens

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,5 +1,19 @@
 const fs = require('fs')
+const path = require('path')
 
-fs.createReadStream('./assets/cachorro.jpg')
-    .pipe(fs.createWriteStream('./assets/salsicha.jpg'))
-    .on('finish', () => console.log('Imagem salva com sucesso'))
+module.exports = (pathFile, fileName, callbackImgCreate) => {
+    const validFiles = ['jpg','png','jpeg']
+    const typeFile = path.extname(pathFile)    
+    const newPath = `./assets/images/${fileName}${typeFile}`
+
+    if(validFiles.indexOf(typeFile.substring(1)) !== -1) {
+        fs.createReadStream(pathFile)
+            .pipe(fs.createWriteStream(newPath))
+            .on('finish', () => callbackImgCreate(false, newPath))
+    } else {
+        const erro = {
+            "msg": "arquivo inválido"
+        }
+        callbackImgCreate(erro)
+    }
+}
--- models/pets.js
+++ models/pets.js
@@ -1,15 +1,30 @@
 const conexao = require('../infraestrutura/conexao')
-
+const uploadDeArquivo = require('../arquivos/uploadDeArquivos')
 class Pet {
     adiciona(pet, res) {
         const sql = "INSERT INTO pets SET ?"
-        conexao.query(sql, pet, err => {
-            if (err) {
-                res.status(400).json(err.sqlMessage)
-            } else {
-                res.status(201).json(pet)
+
+        uploadDeArquivo(pet.imagem, pet.nome, (erro, newPath) => {
+
+            if (erro) {
+                res.status(400).json(erro.msg)
+                return
             }
+
+            const newPet = {
+                nome: pet.nome,
+                imagem: newPath
+            }
+
+            conexao.query(sql, newPet, err => {
+                if (err) {
+                    res.status(400).json(err.sqlMessage)
+                } else {
+                    res.status(201).json(newPet)
+                }
+            })
         })
+
     }
 }
 
